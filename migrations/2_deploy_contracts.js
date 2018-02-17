999fce866fb3771904529521fa04de897258d1b6
Kenton Prescott <kentonprescott@gmail.com>
2018-02-17 10:44:54 -0700

    quick update

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,7 +4,7 @@ const kovanSai = 0x95878488a599e1d821C0fF2Bc079b9e7F96d95bE;
 const kovanTub = 0xa6bfc88aa5a5981a958f9bcb885fcb3db0bf941e;
 
 
-module.exports = function(deployer, network, accounts) {
+module.exports = function(deployer, network) {
 
     if (network == "kovan") {
       deployer.deploy(Ethleverage, kovanTub, kovanSai);
