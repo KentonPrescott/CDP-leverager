ef96c277be31369949486fe7232b00077d03c909
Hernando Castano <castano.ha@gmail.com>
2018-02-17 12:22:36 -0700

    Add Kovan infura endpoint

--- truffle.js
+++ truffle.js
@@ -1,5 +1,8 @@
 // Allows us to use ES6 in our migrations and tests.
 require('babel-register')
+const secrets = require('./secrets.json');
+const infura = require('./infura.json');
+const HDWalletProvider = require('truffle-hdwallet-provider');
 
 module.exports = {
   networks: {
@@ -7,6 +10,14 @@ module.exports = {
       host: '127.0.0.1',
       port: 7545,
       network_id: '*' // Match any network id
-    }
+    },
+    kovan: {
+      gas: 4712388,
+      gasPrice: 10000000000,
+      provider: function() {
+        return new HDWalletProvider(secrets.mnemonic, 'https://kovan.infura.io/' + infura.token)
+      },
+      network_id: 42, // Official ID of the Kovan Network
+    },
   }
 }
