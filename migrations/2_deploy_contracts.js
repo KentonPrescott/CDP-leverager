a8494bdf5a375c172471031ca8afecfdee263265
Kenton Prescott <kentonprescott@gmail.com>
2018-03-22 10:42:47 -0400

    delete testExchange contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
 const CDPOpener = artifacts.require("./CDPOpener.sol");
-const TestExchange = artifacts.require("./TestExchange.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(CDPOpener);
-  //deployer.deploy(TestExchange);
 };
